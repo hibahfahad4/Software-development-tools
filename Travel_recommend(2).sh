7b412e4ca383e0440f1ad143517c9c834ac00ae2
hibahfahad4 <hibahfahad44@gmail.com>
2025-05-19 19:58:10 +0300

    Add files via upload

---
+++ Travel_recommend(2).sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Entertainment section
+"entertainment")
+# Classify number of adults (group)
+if [ "$adults" -gt 9 ]; then
+group="Adults > 9"
+else
+group="Adults ≤ 9"
+fi
+
+echo "Searching for $budget budget destinations for group: $group" 
+print=0
+while read -r line; do
+# Check if the line matches the group
+if [[ "$line" =~ "## $group" ]]; then
+print=1
+continue
+fi
+# If we encounter another section, stop printing
+if [[ "$line" =~ "##" && ! "$line" =~ "$group" ]]; then
+print=0
+fi
+
